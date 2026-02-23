afcbe78bc793965c7f47379ecbce0afede6f3a8c
Gerardo Rodríguez <acidbjazz@gmail.com>
2026-02-23 11:57:16 -0500

    feat: template & action & adobe script

---
+++ script.js
@@ -0,0 +1,4 @@
+const result = document.getElementById("result");
+result.innerHTML = `<p>Adobe Data Layer Test inicializado</p>
+<p>User Agent: ${navigator.userAgent}</p>
+<p>Página cargada correctamente</p>`;
